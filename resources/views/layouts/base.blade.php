32bba9d8d53db7b934928d791d55439819ef6ede
Jeff Anderson <jeffa@bridgetechdynamics.com>
2026-01-19 20:53:21 +0000

    cleaned up links

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -11,18 +11,18 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet">
 
     <!-- Icons -->
-    <link href="./assets/css/nucleo-icons.css" rel="stylesheet">
+    <link href="/assets/css/nucleo-icons.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
 
     <!-- Theme CSS -->
-    <link type="text/css" href="./assets/css/now-design-system-pro.min.css" rel="stylesheet">
+    <link href="/assets/css/now-design-system-pro.min.css" type="text/css" rel="stylesheet">
 
     <!-- Core -->
     <script src="/assets/js/core/popper.min.js"></script>
     <script src="/assets/js/core/bootstrap.min.js"></script>
 
     <!-- Theme JS -->
-    <script src="./assets/js/now-design-system-pro.min.js" type="text/javascript"></script>
+    <script src="/assets/js/now-design-system-pro.min.js" type="text/javascript"></script>
 
 </head>
 
@@ -31,27 +31,27 @@
   @yield('content')
 
   <!--   Core JS Files   -->
-  <script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
-  <script src="./assets/js/core/bootstrap.min.js" type="text/javascript"></script>
-  <script src=""./assets/js/plugins/perfect-scrollbar.min.js"" type="text/javascript"></script>
-  <script src="./assets/js/plugins/moment.min.js"></script>
+  <script src="/assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="/assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+  <script src="/assets/js/plugins/perfect-scrollbar.min.js" type="text/javascript"></script>
+  <script src="/assets/js/plugins/moment.min.js"></script>
 
 
   <!-- Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-  <script src="./assets/js/plugins/nouislider.min.js"></script>
+  <script src="/assets/js/plugins/nouislider.min.js"></script>
 
   <!--  Plugin for the Carousel, full documentation here: http://jedrzejchalubek.com/  -->
-  <script src="./assets/js/plugins/glidejs.min.js"></script>
+  <script src="/assets/js/plugins/glidejs.min.js"></script>
 
   <!--  Plugin for Select, full documentation here: https://joshuajohnson.co.uk/Choices/ -->
-  <script src="./assets/js/plugins/choices.min.js" type="text/javascript"></script>
+  <script src="/assets/js/plugins/choices.min.js" type="text/javascript"></script>
 
 
   <!--  Google Maps Plugin    -->
-  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> -->
 
   <!-- Control Center for Now UI Design System: parallax effects, scripts for the example pages etc -->
-  <script src="./assets/js/now-design-system-pro.min.js" type="text/javascript"></script>
+  <script src="/assets/js/now-design-system-pro.min.js" type="text/javascript"></script>
 
 </body>
 
