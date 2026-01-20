0fd2128e89f03217f8ce49b485c3446f4acb2853
Jeff Anderson <jeffa@bridgetechdynamics.com>
2026-01-20 01:12:35 +0000

    refactored hero 1 for parties

--- resources/views/pages/parties.blade.php
+++ resources/views/pages/parties.blade.php
@@ -3,7 +3,11 @@
 @section('content')
     @include('components.shared.navbar_dark')
     @include('components.shared.navbar_light_2')
-    @include('components.pages.parties.header_1_w_text_and_image_on_right')
+    @include('components.shared.hero_1', [
+        'title'   => 'Party overview hero', 
+        'content' => 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua! Duis aute irure.',
+        'cta_1'   => 'Book Party', 
+    ])
     @include('components.pages.parties.pricing_4')
     @include('components.pages.parties.footer_4_dark_w_2_cols')
     @include('components.shared.faq')
