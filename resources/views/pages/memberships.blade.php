5202c6858cf5c457db92d200e327de8059913930
Jeff Anderson <jeffa@bridgetechdynamics.com>
2026-01-20 02:06:00 +0000

    refactored memberships

--- resources/views/pages/memberships.blade.php
+++ resources/views/pages/memberships.blade.php
@@ -8,10 +8,58 @@
         'content' => 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua! Duis aute irure.',
         'cta_1'   => 'Sign Up', 
     ])
-    @include('components.pages.memberships.features_w_title_and_3_infos')
-    @include('components.pages.memberships.pricing_8')
+
+<section class="py-7">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 mx-auto text-center pb-4">
+        <h2>Benefits of getting a membership</h2>
+        <p class="lead">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua! Duis aute irure. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua! Duis aute irure.</p>
+      </div>
+    </div>
+    <div class="row mt-4">
+      <div class="col-lg-4 col-md-6">
+        <div class="p-3 text-center">
+          <div class="icon icon-shape bg-warning text-center">
+            <i class="ni ni-single-02 opacity-10"></i>
+          </div>
+          <h5 class="mt-4">Lorem Ipsum</h5>
+          <p class="mb-0">Ut enim ad minim veniam, quis nostrud exer</p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="p-3 text-center">
+          <div class="icon icon-shape bg-warning text-center">
+            <i class="ni ni-email-83 opacity-10"></i>
+          </div>
+          <h5 class="mt-4">Lorem Ipsum</h5>
+          <p class="mb-0">Ut enim ad minim veniam, quis nostrud exer</p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 mx-md-auto">
+        <div class="p-3 text-center">
+          <div class="icon icon-shape bg-warning text-center">
+            <i class="ni ni-atom opacity-10"></i>
+          </div>
+          <h5 class="mt-4">Lorem Ipsum</h5>
+          <p class="mb-0">Ut enim ad minim veniam, quis nostrud exer</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+    @include('components.shared.memberships_pricing')
     @include('components.shared.faq')
-    @include('components.pages.memberships.footer_4_dark_w_2_cols')
-    @include('components.pages.memberships.header_2_w_waves_and_typed_text')
+    @include('components.shared.thin_banner', [
+        'title'   => 'Header about viewing policies', 
+        'content' => 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua! Duis aute irure.',
+        'cta_1'   => 'Contact Us', 
+    ])
+    @include('components.shared.bottom_header', [
+        'title'   => 'Header about signing up for a membership', 
+        'content' => 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed do eiusmod tempor incididunt ut labore et dolore.',
+        'cta_1'   => 'Sign Up', 
+    ])
     @include('components.shared.footer')
 @endsection
